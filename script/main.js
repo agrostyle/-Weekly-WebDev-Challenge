cd9d8dfc3777e75ba0aab3b43c8d5f0633b2d665
agrostyle <toma14@wp.pl>
2016-11-27 22:03:47 +0100

    first commit 1

---
+++ script/main.js
@@ -0,0 +1,4 @@
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function (){
+document.querySelector(".nav__menu").classList.toggle("nav__menu--toogle")
+}, false)
